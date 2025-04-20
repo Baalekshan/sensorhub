d8f874e35059c255d64e2c7531aa7fda72e306b9
GnaneshK24 <kgnanesh24@gmail.com>
2025-04-20 20:32:36 +0530

    I3

--- web/next.config.js
+++ web/next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  env: {
-    NODE_ENV: process.env.NODE_ENV || 'development',
-  },
   output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
